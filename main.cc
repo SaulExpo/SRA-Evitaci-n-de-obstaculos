191be63a68411b401c9b1bc3fff3f6169146515b
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-11-22 13:39:16 +0000

    update: main.cc now has a way to tell the robot where to go

--- main.cc
+++ main.cc
@@ -1,33 +1,72 @@
 #include <iostream>
 #include <libplayerc++/playerc++.h>
+#include <cmath>
+
+#define POSITION_TOLERANCE 0.69
+#define ERROR_NO_ARGUMENTS -2
+#define ERROR_PLAYERC -1
+
+
+bool hasArrived(PlayerCc::Position2dProxy* position, const double destinyX, const double destinyY, const double destinyYaw)
+{  
+  return std::abs(destinyX - position->GetXPos())  < POSITION_TOLERANCE &&
+         std::abs(destinyY - position->GetYPos())   < POSITION_TOLERANCE &&
+         std::abs(destinyYaw - position->GetYaw()) < POSITION_TOLERANCE;
+}
+
+void moveTo(PlayerCc::PlayerClient* client, PlayerCc::Position2dProxy* algorithmProxy, const double X, const double Y, const double Yaw)
+{
+  algorithmProxy->GoTo(X, Y, Yaw);
+  while(!hasArrived(algorithmProxy, X, Y, Yaw)){
+    algorithmProxy->RequestGeom();
+    client->Read();
+
+    //std::cout << "X: " << algorithmProxy->GetXPos() << std::endl;
+    //std::cout << "Y: " << algorithmProxy->GetYPos() << std::endl;
+    //std::cout << "Yaw: " << algorithmProxy->GetYaw() << std::endl;
+
+    algorithmProxy->GoTo(X - algorithmProxy->GetXPos(), Y - algorithmProxy->GetYPos(), Yaw - algorithmProxy->GetYaw());
+   }
+ 
+}
 
 
 int main(int argc, char *argv[]){
   std::string player_hostname = PlayerCc::PLAYER_HOSTNAME;
   int player_port = PlayerCc::PLAYER_PORTNUM;
 
+  if (argc < 4){
+    std::cerr << "Usage: " << argv[0] << " X Y Yaw [hostname] [port]" << std::endl;
+    return ERROR_NO_ARGUMENTS;
+   }
   
-  const double speed = 0.1;
-  const int rotation_radians = PlayerCc::dtor(0);
+  const double X = std::stod(argv[1]);
+  const double Y = std::stod(argv[2]);
+  const double Yaw = std::stod(argv[3]);
+  
+  if (argc == 5)
+    player_hostname = argv[4];
+  if(argc == 6)
+    player_port = std::stoi(argv[5]);
+
   try 
   {
     PlayerCc::PlayerClient robot{player_hostname, player_port};
-    PlayerCc::Position2dProxy positionProxy{&robot, 0};
     PlayerCc::Position2dProxy algorithmProxy{&robot, 1};
 
     PlayerCc::LogProxy logProxy{&robot, 0};
     logProxy.SetState(1);
     logProxy.SetWriteState(1);
 
-    PlayerCc::player_pose2d
+    moveTo(&robot, &algorithmProxy, X, Y, Yaw);
+    std::cout << "Arrived to destiny" << std::endl;
 
     logProxy.SetWriteState(0);
-    positionProxy.SetSpeed(0.0, 0.0);
   }
   catch(PlayerCc::PlayerError& e)
   {
     std::cerr << e << std::endl;
-    return -1;
+    return ERROR_PLAYERC;
   }
 
   return 0;
